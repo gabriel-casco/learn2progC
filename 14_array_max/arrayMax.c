2f08a7cb8a80553a4130867750e3ca542ebe5abd
Balakrishna Naidu <bkrishna8916@gmail.com>
2020-06-06 19:25:30 +0000

    Complete Assignment 14_array_max

--- 14_array_max/arrayMax.c
+++ 14_array_max/arrayMax.c
@@ -2,7 +2,18 @@
 #include <stdlib.h>
 
 int * arrayMax(int * array, int n) {
-  return NULL;
+  if (n == 0) {
+    return NULL;
+  }
+  else {
+    int *max = array;
+    for (int i = 0; i < n; i++) {
+      if (array[i] > *max) {
+	*max = array[i];
+      }
+    }
+    return max;
+  }
 }
 
 void doTest(int * array, int n) {
