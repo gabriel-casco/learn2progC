71264db93bf1f19a379c29a290009be783c622f7
Balakrishna Naidu <bkrishna8916@gmail.com>
2020-06-10 07:55:23 +0000

    25

--- 25_break_encr/breaker.c
+++ 25_break_encr/breaker.c
@@ -1,56 +1,89 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <ctype.h>
+#include <string.h>
 
-int findMaxIdx(int array[], int n)
-{
-  int maxIdx = 0;
-  int i;
+#define LINE_SIZE 4096
 
-  for (i = 0; i < n; i++) {
-    if (array[i] > array[maxIdx]) {
-      maxIdx = i;
-    }
-  }
-  return maxIdx;
-}
+int freqCount(FILE * f, size_t * count);
+unsigned largestIndex(size_t * count);
 
-int getkey(FILE *f)
-{
-  int freq[26] = {0};
-  int ch;
-  int maxIdx;
-
-  while ((ch = fgetc(f)) != EOF) {
-    if (isalpha(ch)) {
-      freq[tolower(ch) - 'a']++;
-    }
-  }
-  maxIdx = findMaxIdx(freq, 26);
-  /* transform is 4 (e) -> maxIdx */
-  /* or x -> (x + (maxIdx - 4)) % 26 */
-  return (maxIdx + 22) % 26;
-}
-
-int main(int argc, char *argv[])
-{
-  FILE *f;
-  int key;
-
+int main(int argc, char ** argv) {
   if (argc != 2) {
-    fprintf(stderr, "usage %s file\n", argv[0]);
-    exit(EXIT_FAILURE);
+    fprintf(stderr, "Wrong number of input files: %d\n", argc);
+    return EXIT_FAILURE;
   }
 
-  f = fopen(argv[1], "r");
+  FILE * f = fopen(argv[1], "r");
+
   if (f == NULL) {
-    fprintf(stderr, "failed to open file %s\n", argv[1]);
-    exit(EXIT_FAILURE);
+    perror("Could not open file");
+    return EXIT_FAILURE;
   }
 
-  key = getkey(f);
-  printf("%d\n", key);
 
-  fclose(f);
-  return 0;
+  size_t count[26] = {0}; // count[] contains counts of each letter
+
+  int countSuccess = freqCount(f,count);
+  if (countSuccess == EXIT_FAILURE) {
+    return EXIT_FAILURE;
+  }
+
+  unsigned id = largestIndex(count); // id should be in [0, 26)
+
+  int k = (id - 4 + 26) % 26;
+
+  printf("%d\n", k);
+
+
+
+  if (fclose(f) != 0) {
+    perror("Failed to close the input file!");
+    return EXIT_FAILURE;
+  }
+
+  return EXIT_SUCCESS;
+}
+
+// read each line from file and count frequency of each letter
+
+int freqCount(FILE * f, size_t * count) {
+
+  // using fgetc
+  int c = fgetc(f);
+  size_t nletter = 0;
+  if (c == EOF) {
+    perror("Empty file!\n");
+    return EXIT_FAILURE;
+  }
+
+  do {
+    if (isalpha(c)) {
+      c = tolower(c);
+      c -= 'a';
+      count[c]++;
+      nletter++;
+    }
+  } while ((c = fgetc(f)) != EOF);
+
+  if (nletter == 0) {
+    perror("No letters in file!\n");
+    return EXIT_FAILURE;
+  }
+
+  return EXIT_SUCCESS;
+}
+
+unsigned largestIndex(size_t * count) {
+
+  size_t temp = count[0];
+  unsigned id = 0;
+
+  for (size_t i = 1; i < 26; i++) {
+    if(temp < count[i]) {
+      temp = count[i];
+      id = i;
+    }
+  }
+  return id;
 }
