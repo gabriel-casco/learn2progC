efc293cb0fa9c08e170352d442abef21915f09ef
Balakrishna Naidu <bkrishna8916@gmail.com>
2020-06-10 07:31:45 +0000

    25 assignment

--- 25_break_encr/breaker.c
+++ 25_break_encr/breaker.c
@@ -2,55 +2,55 @@
 #include <stdlib.h>
 #include <ctype.h>
 
-int findkey(FILE *f){
-  int arr[26]={0};
-  int c;
+int findMaxIdx(int array[], int n)
+{
+  int maxIdx = 0;
+  int i;
 
-  while((c=fgetc(f)) != EOF){
-    if (isalpha(c)) {
-      c=tolower(c);
-      c -= 'a';
-      arr[c] ++;
+  for (i = 0; i < n; i++) {
+    if (array[i] > array[maxIdx]) {
+      maxIdx = i;
     }
   }
-  int max=0;
-  int j=0;
-  for (int i=0 ;i<26; i++){
-    if(arr[i] > max){
-      max=arr[i];
-      j=i;}
-  }
-  int k =  0;
-  if (j >= ('e' - 'a')) k=j-('e'-'a') ;
-  else k=(26-('e'-'a'))+ j;
-  return k ;
+  return maxIdx;
 }
 
+int getkey(FILE *f)
+{
+  int freq[26] = {0};
+  int ch;
+  int maxIdx;
 
+  while ((ch = fgetc(f)) != EOF) {
+    if (isalpha(ch)) {
+      freq[tolower(ch) - 'a']++;
+    }
+  }
+  maxIdx = findMaxIdx(freq, 26);
+  /* transform is 4 (e) -> maxIdx */
+  /* or x -> (x + (maxIdx - 4)) % 26 */
+  return (maxIdx + 22) % 26;
+}
 
+int main(int argc, char *argv[])
+{
+  FILE *f;
+  int key;
 
-
-int main(int argc,char** argv){
-  if (argc != 2 ){
-    fprintf(stderr,"not enough arguments\n");
-    return EXIT_FAILURE;
+  if (argc != 2) {
+    fprintf(stderr, "usage %s file\n", argv[0]);
+    exit(EXIT_FAILURE);
   }
 
-  FILE * f= fopen(argv[1] ,"r");
-
+  f = fopen(argv[1], "r");
   if (f == NULL) {
-    perror("Could not open file");
-    return EXIT_FAILURE;
+    fprintf(stderr, "failed to open file %s\n", argv[1]);
+    exit(EXIT_FAILURE);
   }
 
-  int key= findkey(f);
-  printf("%d\n",key);
+  key = getkey(f);
+  printf("%d\n", key);
 
-  if (fclose(f) != 0) {
-    perror("Failed to close the input file!");
-    return EXIT_FAILURE;
-  }
-
-  return EXIT_SUCCESS;
-
+  fclose(f);
+  return 0;
 }
